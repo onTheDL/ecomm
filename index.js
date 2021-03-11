89036807d01896f6ad7997516e2dd6ac4ac51ba1
Daniel Laserna <daniel.laserna@protonmail.com>
2021-03-11 18:12:17 -0500

    Add POST event handler, form parsing

--- index.js
+++ index.js
@@ -4,19 +4,36 @@ const express = require("express");
 const app = express();
 
 // ROUTE HANDLERS
-app.get('/', ((req, res) => {
+app.get("/", (req, res) => {
   res.send(`
     <div>
-      <form>
-        <input placeholder="email" />
-        <input placeholder="password" />
-        <input placeholder="password confirmation" />
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
         <button>Sign Up</button>
       </form>
     </div>
-  `)
-}))
+  `);
+});
+
+app.post("/", (req, res) => {
+  // get access to email, password, passwordConfirmation
+
+  // on = an event listener
+  req.on("data", (data) => {
+    const parsed = data.toString("utf8").split("&");
+    const formData = {};
+    for (let pair of parsed) {
+      const [key, value] = pair.split("=");
+      formData[key] = value;
+    }
+    console.log(formData);
+  });
+
+  res.send("Account created!!");
+});
 
 app.listen(3000, () => {
-  console.log('Listening')
-})
+  console.log("Listening");
+});
