b80d5f0a1c4d259678f75c03bbed77d388572724
Daniel Laserna <daniel.laserna@protonmail.com>
2021-03-12 23:38:52 -0500

    Add getOne(), delete(), update()

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-const crypto = require('crypto')
+const crypto = require("crypto");
+const { log } = require("console");
 
 class UsersRepository {
   // N.B. constructors are not allowed to be asyncronous
@@ -28,31 +29,72 @@ class UsersRepository {
   }
   // attr = attributes
   async create(attrs) {
-    attrs.id = this.randomId()
+    attrs.id = this.randomId();
 
-    const records = await this.getAll()
-    records.push(attrs)
+    const records = await this.getAll();
+    records.push(attrs);
 
-    await this.writeAll(records)
+    await this.writeAll(records);
   }
 
   async writeAll(records) {
-    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2))
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
   }
 
   randomId() {
-    return crypto.randomBytes(4).toString('hex')
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((record) => record.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true
+      
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false
+        }
+      }
+      if (found) {
+        return record;
+      }
+    }
   }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
 
-  await repo.create({ email: 'test@test.com', password: 'password' })
+  const user = await repo.getOneBy({ email: "test@test.com" });
 
-  const users = await repo.getAll();
-
-  console.log(users);
+  console.log(user);
 };
 
 test();
