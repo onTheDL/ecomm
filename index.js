ccc0a646156d96f646902bf1d3050ba0b23bb98a
Daniel Laserna <daniel.laserna@protonmail.com>
2021-03-13 21:54:44 -0500

    Add signup validation logic

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const usersRepo = require("./repositories/users");
 // Describes what our web servers can do
 const app = express();
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // ROUTE HANDLERS
 app.get("/", (req, res) => {
@@ -17,19 +21,38 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", (req, res) => {
+/*
+// Middleware
+const bodyParser = (req, res, next) => {
   // get access to email, password, passwordConfirmation
-
   // on = an event listener
-  req.on("data", (data) => {
-    const parsed = data.toString("utf8").split("&");
-    const formData = {};
-    for (let pair of parsed) {
-      const [key, value] = pair.split("=");
-      formData[key] = value;
-    }
-    console.log(formData);
-  });
+
+  if (req.method === "POST") {
+    req.on("data", (data) => {
+      const parsed = data.toString("utf8").split("&");
+      const formData = {};
+      for (let pair of parsed) {
+        const [key, value] = pair.split("=");
+        formData[key] = value;
+      }
+      req.body = formData
+      next()
+    });
+  } else next()
+};
+*/
+
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords must match");
+  }
 
   res.send("Account created!!");
 });
