a88721ff86d72efb5e6c0a96d80b0625a4c9a648
Daniel Laserna <daniel.laserna@protonmail.com>
2021-03-18 18:19:20 -0400

    Refactor project structure

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser"); // a middleware fxn
 const cookieSession = require("cookie-session"); // a middleware fxn
-const usersRepo = require("./repositories/users");
+const authedRouter = require("./routes/admin/auth");
 
 // Describes what our web servers can do
 const app = express();
 
 // MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cookieSession({
-    keys: ["fsadflj233j"],
-  })
-);
-
-// ROUTE HANDLERS
-app.get("/signup", (req, res) => {
-  res.send(`
-    <div>
-      Your ID is: ${req.session.userId}
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `);
-});
 
 /*
 // Middleware
@@ -50,65 +30,13 @@ const bodyParser = (req, res, next) => {
 };
 */
 
-app.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
+app.use(
+  cookieSession({
+    keys: ["fsadflj233j"],
+  })
+);
 
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send("Email in use");
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.send("Passwords must match");
-  }
-
-  // Create a user in our user repo to represent this person
-  const user = await usersRepo.create({ email, password });
-
-  // Store the id of that user inside the user's cookie
-  req.session.userId = user.id;
-
-  res.send("Account created!!");
-});
-
-app.get("/signout", (req, res) => {
-  req.session = null;
-  res.send("You are logged out.");
-});
-
-app.get("/signin", (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <button>Sign In</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await usersRepo.getOneBy({ email });
-
-  if (!user) {
-    return res.send("Email not found");
-  }
-
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  )
-  if (!validPassword) {
-    return res.send("Invalid password")
-  }
-
-  req.session.userId = user.id;
-
-  res.send('You are signed in!')
-});
+app.use(authedRouter);
 
 app.listen(3000, () => {
   console.log("Listening");
