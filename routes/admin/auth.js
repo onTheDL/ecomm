fdac42a1b158d3b197629b9f1a7e524eb3f3e472
Daniel Laserna <daniel.laserna@protonmail.com>
2021-03-21 17:37:35 -0400

    Display error message in ui

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -22,7 +22,10 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
+    
+    if (!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }))
+    }
 
     const { email, password, passwordConfirmation } = req.body;
 
