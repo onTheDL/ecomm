8c56ab6e43e40e045a1c1b3db24a7a1208d46f60
Daniel Laserna <daniel.laserna@protonmail.com>
2021-03-24 14:46:01 -0400

    Create base Repository class

---
+++ repositories/repositories.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+module.exports = class Repository {
+  // N.B. constructors are not allowed to be asyncronous
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("Creating a repository requires a filename");
+    }
+    this.filename = filename;
+    // checking to see if filename exists
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+
+  // Outside of constructors, ideal to work with Promises as much as possible
+
+  async create(attrs) {
+    attrs.id = this.randomId();
+
+    const records = await this.getAll();
+    records.push(attrs);
+    await this.writeAll(records);
+
+    return attrs
+  }
+
+  async getAll() {
+    // Open the file called this.filename
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: "utf8",
+      })
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((record) => record.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
+
+  async getOneBy(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
+        }
+      }
+      if (found) {
+        return record;
+      }
+    }
+  }
+};
