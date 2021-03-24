1c0f37a0a5b931db54843c47001a68b52a4f9d36
Daniel Laserna <daniel.laserna@protonmail.com>
2021-03-24 16:46:32 -0400

    Use multer to access uploaded image

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
-const router = express.Router();
+const multer = require("multer");
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
+const router = express.Router();
 router.get("/admin/products", (req, res) => {});
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
+router.post(
+  "/admin/products/new",
+  [requireTitle, requirePrice],
+  upload.single("image"),
+  (req, res) => {
+    const errors = validationResult(req);
 
-  res.send("submitted");
-});
+    console.log(req.file);
+
+    res.send("submitted");
+  }
+);
 
 module.exports = router;
