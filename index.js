bf2e15c50c36b8ae3453002eea23913cfb3364a1
Daniel Laserna <daniel.laserna@protonmail.com>
2021-03-23 20:36:47 -0400

    Add styling and public folder

--- index.js
+++ index.js
@@ -6,29 +6,9 @@ const authedRouter = require("./routes/admin/auth");
 // Describes what our web servers can do
 const app = express();
 
-// MIDDLEWARE
+// MIDDLEWARES
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-/*
-// Middleware
-const bodyParser = (req, res, next) => {
-  // get access to email, password, passwordConfirmation
-  // on = an event listener
-
-  if (req.method === "POST") {
-    req.on("data", (data) => {
-      const parsed = data.toString("utf8").split("&");
-      const formData = {};
-      for (let pair of parsed) {
-        const [key, value] = pair.split("=");
-        formData[key] = value;
-      }
-      req.body = formData
-      next()
-    });
-  } else next()
-};
-*/
 
 app.use(
   cookieSession({
