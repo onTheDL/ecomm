074ec22bd0e579f83bd068fb961360bb2a3ed902
Daniel Laserna <daniel.laserna@protonmail.com>
2021-03-29 20:12:39 -0400

    Add edit functionality with error handling

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -2,17 +2,23 @@ const { validationResult } = require("express-validator");
 
 module.exports = {
   //N.B.  all middlewares that requires customization must be a fxn that returns a fxn
-  handleErrors(templateFunc) {
-    return (req, res, next) => {
+  handleErrors(templateFunc, dataCb) {
+    return async (req, res, next) => {
       const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
-        return res.send(templateFunc({ errors }));
+
+        // Using '...' prevents from spreading an undefined value (ln 16)
+        let data = {};
+        if (dataCb) {
+          data = await dataCb(req)
+        }
+        return res.send(templateFunc({ errors, ...data }));
       }
       next();
     };
   },
-  // A middleware that does not require customization
+  // A middleware that does not require customization does not need to return a fxn
   requireAuth(req, res, next) {
     if (!req.session.userId) {
       return res.redirect("/signin");
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -6,7 +6,7 @@ const { handleErrors, requireAuth } = require("./middlewares");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const productsIndexTemplate = require("../../views/admin/products/index");
-const productsEditTemplate = require("../../views/admin/products/edit")
+const productsEditTemplate = require("../../views/admin/products/edit");
 
 const { requireTitle, requirePrice } = require("./validators");
 
@@ -53,8 +53,30 @@ router.get("/admin/products/:id/edit", requireAuth, async (req, res) => {
   res.send(productsEditTemplate({ product }));
 });
 
-router.post('/admin/products/:id/edit', requireAuth, async (req, res) => {
-  
-})
+router.post(
+  "/admin/products/:id/edit",
+  requireAuth,
+  upload.single("image"),
+  [requireTitle, requirePrice],
+  handleErrors(productsEditTemplate, async (req) => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
+  async (req, res) => {
+    const changes = req.body;
+
+    if (req.file) {
+      changes.image = req.file.buffer.toString("base64");
+    }
+
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send("Could not find item");
+    }
+
+    res.redirect("/admin/products");
+  }
+);
 
 module.exports = router;
