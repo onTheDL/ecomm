6e78de6d157b7d4b66f59f4cdde8fe76387a50bc
Daniel Laserna <daniel.laserna@protonmail.com>
2021-04-01 17:38:41 -0400

    Add initial create cart functionality

--- routes/carts.js
+++ routes/carts.js
@@ -1,17 +1,32 @@
 const express = require("express");
+const cartsRepo = require("../repositories/carts");
 
 const router = express.Router();
 
 // Receive a POST request to add an item to a cart
-router.post("/cart/products", (req, res) => {
-  console.log(req.body.productId)
+router.post("/cart/products", async (req, res) => {
+  // Identify the cart
+  let cart;
+  if (!req.session.cartId) {
+    // cart doesn't exist --> need to create one
+    // store cart id in req.session.cartId property
 
-  res.send("Product added to cart")
-})
+    cart = await cartsRepo.create({ items: [] });
 
+    req.session.cartId = cart.id;
+  } else {
+    // we have a cart --> get from repository
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+
+  console.log(cart)
+  // Either increment quantity of existing product, OR add new product to items array
+
+  res.send("Product added to cart");
+});
 
 // Receive a GET request to show all items in cart
 
 // Receive a POST request to delete an item from a cart
 
-module.exports = router
+module.exports = router;
