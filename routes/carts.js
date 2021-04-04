8be985ab8f625558e42e535a56af393d12afeea3
Daniel Laserna <daniel.laserna@protonmail.com>
2021-04-03 23:57:15 -0400

    Add functionality to delete an item from cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cartsRepo = require("../repositories/carts");
-const productsRepo = require('../repositories/products')
-const cartShowTemplate = require('../views/carts/show')
+const productsRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
 
 const router = express.Router();
 
@@ -37,7 +37,7 @@ router.post("/cart/products", async (req, res) => {
     items: cart.items,
   });
 
-  res.send("Product added to cart");
+  res.redirect("/cart");
 });
 
 // 2. Receive a GET request to show all items in cart
@@ -45,16 +45,26 @@ router.get("/cart", async (req, res) => {
   if (!req.session.cartId) {
     return res.redirect("/");
   }
-  const cart = await cartsRepo.getOne(req.session.cartId)
+  const cart = await cartsRepo.getOne(req.session.cartId);
 
   for (let item of cart.items) {
-    const product = await productsRepo.getOne(item.id)
+    const product = await productsRepo.getOne(item.id);
 
-    item.product = product
+    item.product = product;
   }
-  res.send(cartShowTemplate({ items: cart.items }))
+  res.send(cartShowTemplate({ items: cart.items }));
 });
 
 // 3. Receive a POST request to delete an item from a cart
+router.post("/cart/products/delete", async (req, res) => {
+  const { itemId } = req.body;
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter((item) => item.id !== itemId);
+
+  await cartsRepo.update(req.session.cartId, { items });
+
+  res.redirect('/cart')
+});
 
 module.exports = router;
--- views/carts/show.js
+++ views/carts/show.js
@@ -1,18 +1,17 @@
-const layout = require('../layout');
+const layout = require("../layout");
 
 module.exports = ({ items }) => {
-
   // let totalPrice = 0
   // for (let item of items) {
   //   totalPrice += item.quantity * item.product.price
   // }
 
   const totalPrice = items.reduce((prev, item) => {
-    return prev + item.quantity * item.product.price
-  }, 0)
+    return prev + item.quantity * item.product.price;
+  }, 0);
 
   const renderedItems = items
-    .map(item => {
+    .map((item) => {
       return `
         <div class="cart-item message">
           <h3 class="subtitle">${item.product.title}</h3>
@@ -24,7 +23,8 @@ module.exports = ({ items }) => {
               $${item.product.price * item.quantity}
             </div>
             <div class="remove">
-              <form method="POST">
+              <form method="POST" action="/cart/products/delete">
+                <input hidden value=${item.id} name="itemId" />
                 <button class="button is-danger">                  
                   <span class="icon is-small">
                     <i class="fas fa-times"></i>
@@ -36,7 +36,7 @@ module.exports = ({ items }) => {
         </div>
       `;
     })
-    .join('');
+    .join("");
 
   return layout({
     content: `
@@ -59,6 +59,6 @@ module.exports = ({ items }) => {
           <div class="column"></div>
         </div>
       </div>
-    `
+    `,
   });
 };
